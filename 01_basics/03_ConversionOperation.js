16ace86fa1b7cee98865a18e7d839f1fdc88121f
dalip03 <dalipdivakar@gmail.com>
2024-03-28 00:32:39 +0530

    operations

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -1,11 +1,11 @@
 let score = "40"
 
-console.log(typeof score);
-console.log(typeof(score));   // call as method
+//console.log(typeof score);
+//console.log(typeof(score));   // call as method
 
 // change string to number using Number
 let valueInNumber = Number(score);
-console.log( " After Conversion : " + typeof(valueInNumber) );
+ // console.log( " After Conversion : " + typeof(valueInNumber) );
 
 // "44" convert 44
 //  "44ab" convert  nan
@@ -14,8 +14,32 @@ let isLoggedIn =1 // convert true
 let isLoggedIn1 ="" // convert false
 let isLoggedIn2 ="dalip" // convert true
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+ // console.log(booleanIsLoggedIn);
 
 let someNumber =44
 let stringNumber = String(someNumber)
-console.log( typeof stringNumber);
+// console.log( typeof stringNumber);
+
+
+let value = 4
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = "Dalip"
+let str3 = str1 + str2
+ // console.log(str3);
+
+ // console.log("1" + 2 + 2);   //122  if string is at first then it traet all as string 
+ // console.log(1 + 2 + "4");  // 34   if number is at first then treat axactly
+ // console.log((3 + 4 ) * 5 % 3); //2
+
+// console.log(+true);    
+// let num1 , num2 , num3 
+// num1 = num2 = num3 = 2 + 2    not preferable
+
+let gameCounter = 100
+gameCounter++
+// ++gameCounter
+console.log(gameCounter);
+
