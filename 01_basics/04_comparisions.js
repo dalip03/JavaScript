67b71f57584127c22c7dba4bf4a29a1ae99ed18b
dalip03 <dalipdivakar@gmail.com>
2024-03-28 00:40:28 +0530

    compare

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,11 @@
+// > , >= , <AbortController, ==, !=
+
+// console.log("2" > 1);  // not allowed
+// console.log(null == 0);  // false
+// console.log(null >= 0); // true
+console.log(undefined == 0);  // false 
+
+// value and datatype check by ====
+
+console.log("22" === 2); //  false bcz data type is not same 
+console.log(2 === 2); // true
