485007bcfc6dd7e19179dfca04ef8535e5f01ff5
dalip03 <dalipdivakar@gmail.com>
2024-03-28 00:02:13 +0530

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"Use strict";  // treat all js code as newer version
+
+// alert(4+4)  => not working , bcs we use node js not browser
+// code such a way that code readability increase
+
+// console.log(4 +4);
+
+let name = "Dalip"
+let age = 23
+let isLoggedIn = false
+let state = null
+
+//Data Types in JS
+// ..............Premitive Data Types..............
+//  number => 2 to power 53
+//  bigint
+//  string => " "
+//  boolean => true /false
+//  null => standalone value    // empty value 
+//  undefined => not defined
+//  symbol => unique 
+
+// ..........Objects ..............
+
+console.log(typeof age); // it return number type 
+console.log(typeof null);  // it retuns object type
+console.log(typeof undefined); // it returns undefined type 
+
